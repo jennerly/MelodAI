eeeed062e0b9b1807acc4f6116c69cdb131a3fbb
Jenna <jennasaykhamphone.js@gmail.com>
2023-08-01 11:36:38 -0400

    Initial Commit

---
+++ MelodAI/MelodAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodAIApp.swift
+//  MelodAI
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MelodAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
