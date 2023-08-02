c9a82957eae9d6b7c21f3eae5c0de162c269ed6a
Jenna <jennasaykhamphone.js@gmail.com>
2023-08-02 13:52:50 -0400

    editing the background of sad

--- SadView.swift
+++ SadView.swift
@@ -8,38 +8,87 @@
 import SwiftUI
 
 struct SadView: View {
-    @State private var sadArray = ""
-    @State private var sadSongArray = ["song1", "song2", "song3"]
-    @State private var sadPodcastArray = ["podcast1", "podcast2", "podcast3"]
-    @State private var sadNoiseArray = ["noise1", "noise2", "noise3"]
+    @State private var sadSongArray = false
+    @State private var sadPodcastArray = false
+    @State private var sadNoiseArray = false
     var body: some View {
-        ZStack{
-            VStack{
-                HStack{
-                    Text("MelodAI")
-                    Text("User")
-                }
-                Text("Sad")
-                
-                HStack{
-                    Button("Songs") {
-                        sadArray = sadSongArray
-                    } //songs
+        NavigationStack{
+            ZStack {
+                Image("darkBluetoLight")
+                    .resizable(resizingMode: .stretch)
+                    .ignoresSafeArea()
+                VStack{
+                    HStack{
+                        Text("MelodAI")
+                            .font(.title)
+                        Spacer()
+                        Text("User")
+                            .fontWeight(.regular)
+                    } .padding()
+                    Spacer()
+                    Text("Sad")
                     
-                    Button("Podcasts") {
-                        sadArray = sadPodcastArray
-                    } //podcasts
-                    
-                    Button("Noises") {
-                        sadArray = sadNoiseArray
-                    } //noise
-                } //hstack
-                Spacer()
-                Text(sadArray)
-            } //vstack
-        } //zstack
-    } //someview
-} //struct
+                    HStack{
+                        Button("Songs") {
+                            sadSongArray = true
+                            sadPodcastArray = false
+                            sadNoiseArray = false
+                        } //songs
+                        
+                        Button("Podcasts") {
+                            sadPodcastArray = true
+                            sadSongArray = false
+                            sadNoiseArray = false
+                        } //podcasts
+                        
+                        Button("Noises") {
+                            sadNoiseArray = true
+                            sadSongArray = false
+                            sadPodcastArray = false
+                        } //noise
+                    } //hstack
+                    Spacer()
+                    if sadSongArray == true {
+                        List {
+                            Text("song1")
+                            Text("song2")
+                            Text("song3")
+                            Text("song4")
+                            Text("song5")
+                        }
+                    } else if sadPodcastArray == true {
+                        List {
+                            Text("podcast1")
+                            Text("podcast2")
+                            Text("podcast3")
+                            Text("podcast4")
+                            Text("podcast5")
+                        }
+                    } else if sadNoiseArray == true {
+                        List {
+                            Text("noise1")
+                            Text("noise2")
+                            Text("noise3")
+                            Text("noise4")
+                            Text("noise5")
+                            
+                        }
+                    }
+                } //vstack
+                .toolbar {
+                    ToolbarItemGroup(placement: .status){
+                        NavigationLink(destination: HomeView()) {
+                            Text("🏠")
+                                .font(.title)
+                            Text("👤")
+                        } // closing nav link
+                    } // closing toolbaritemgroup
+                } // closing tool bar
+            } //closing nav stack
+            .toolbarBackground(Color("HotOrange"), for: .navigationBar)
+        } //closing zstack
+    } //closing someview
+} //closing struct
 
 struct SadView_Previews: PreviewProvider {
     static var previews: some View {
